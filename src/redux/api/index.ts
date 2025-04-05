fd907d2c6da4738af9ade7873ffef1f7c1f717f5
EmirlanDev <frdevemirlan@gmail.com>
2025-04-05 12:45:49 +0600

    baseUrl edit

--- src/redux/api/index.ts
+++ src/redux/api/index.ts
@@ -5,7 +5,7 @@ import {
 } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://platform-back-qgul.onrender.com/api",
+  baseUrl: "/api",
   credentials: "include",
 });
 
